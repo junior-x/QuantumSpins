6592242036195af68f1ecc49e3a87c4c31b3b3fb
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-29 22:45:32 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -15,3 +15,22 @@ module.exports = {
     }),
   },
 };
+
+
+///SASS IMPORT CONFIG
+
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-sass-transformer")
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "scss", "sass"]
+    }
+  };
+})();
